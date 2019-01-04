1cd2f5b5741b38bd5e69b4b7bf6ad72f3c2f469b
igor-kuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-01-04 12:06:38 +0100

    Rebase

--- main.old/scripts.js
+++
@@ -1,9 +0,0 @@
-var nav = document.getElementsByTagName("nav")[0];
-var tabs = nav.getElementsByTagName("li");
-for (var i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function() {
-      var active = document.getElementsByClassName("active")[0];
-      active.className = active.className.replace("active", "");
-      this.className += "active";
-  });
-}
