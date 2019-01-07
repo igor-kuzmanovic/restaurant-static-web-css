c8fea0f1f89b39f7d8dc9eb9a96db7b8bada7401
igor-kuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-01-07 10:38:57 +0100

    Installed gruntjs and node-sass, initial index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,64 @@
-module.exports = function(grunt) {
-  
-  grunt.registerTask("default", "", function() {
-    grunt.log.write("Grunt test");
+module.exports = function (grunt) {
+
+  const sass = require('node-sass');
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      sass: {
+        files: 'sass/*.scss',
+        tasks: ['sass', 'cssmin']
+      },
+      js: {
+        files: ['js/*.js', 'js/scripts.js', '!js/scripts.min.js'],
+        tasks: ['concat', 'uglify']
+      }
+    },
+
+    sass: {
+      options: {
+        implementation: sass,
+        style: 'expanded'
+      },
+      dist: {
+        files: { 
+          'css/styles.css' : 'sass/styles.scss'
+        }
+      }
+    }, 
+
+    cssmin: {
+      target: {
+        files: {
+          'css/styles.min.css' : 'css/styles.css'
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        serparator: ';',
+      },
+      dist: {
+        files: {
+          'js/scripts.js' : ['js/*.js', '!js/scripts.js']
+        }
+      }
+    },
+
+    uglify: {
+      target: {
+        files: { 
+          'js/scripts.min.js' : 'js/scripts.js'
+        }
+      }
+    }
+
   });
-  
+
+  grunt.registerTask('default', ['watch']);
+
 };
